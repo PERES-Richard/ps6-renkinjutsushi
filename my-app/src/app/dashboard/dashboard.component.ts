3178d9b8bb1171a356e6781cedf3a1f6c6bbfeb1
PC-THOMAS <thomas.colomban@etu.unice.fr>
2019-04-14 09:30:44 +0200

    Adding graph stats

--- my-app/src/app/dashboard/dashboard.component.ts
+++ my-app/src/app/dashboard/dashboard.component.ts
@@ -3,75 +3,25 @@ import * as Chartist from 'chartist';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
+  templateUrl: '../../../../../../WebstormProjects/ps6-renkinjutsushi/my-app/src/app/dashboard/dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
-  startAnimationForLineChart(chart) {
-    let seq: any, delays: any, durations: any;
-    seq = 0;
-    delays = 80;
-    durations = 500;
+  constructor() {
+  }
 
-    chart.on('draw', function (data) {
-      if (data.type === 'line' || data.type === 'area') {
-        data.element.animate({
-          d: {
-            begin: 600,
-            dur: 700,
-            from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
-            to: data.path.clone().stringify(),
-            easing: Chartist.Svg.Easing.easeOutQuint
-          }
-        });
-      } else if (data.type === 'point') {
-        seq++;
-        data.element.animate({
-          opacity: {
-            begin: seq * delays,
-            dur: durations,
-            from: 0,
-            to: 1,
-            easing: 'ease'
-          }
-        });
-      }
-    });
-
-    seq = 0;
-  };
-  startAnimationForBarChart(chart) {
-    let seq2: any, delays2: any, durations2: any;
-
-    seq2 = 0;
-    delays2 = 80;
-    durations2 = 500;
-    chart.on('draw', function (data) {
-      if (data.type === 'bar') {
-        seq2++;
-        data.element.animate({
-          opacity: {
-            begin: seq2 * delays2,
-            dur: durations2,
-            from: 0,
-            to: 1,
-            easing: 'ease'
-          }
-        });
-      }
-    });
-
-    seq2 = 0;
-  };
   ngOnInit() {
-    /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
+
+
+    /**
+     *   Succeed Graph Init
+     */
 
     const dataDailySalesChart: any = {
-      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+      labels: ['2012', '2013', '2014', '2015', '2016', '2017', '2018'],
       series: [
-        [12, 17, 7, 17, 23, 18, 38]
+        [80, 82, 75, 85, 80, 76, 72]
       ]
     };
 
@@ -79,72 +29,80 @@ export class DashboardComponent implements OnInit {
       lineSmooth: Chartist.Interpolation.cardinal({
         tension: 0
       }),
-      low: 0,
-      high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-      chartPadding: { top: 0, right: 0, bottom: 0, left: 0 },
+      low: 50,
+      high: 100,
+      chartPadding: {top: 0, right: 0, bottom: 0, left: 0},
     }
 
-    const dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
-
-    this.startAnimationForLineChart(dailySalesChart);
+    const sucessRate = new Chartist.Line('#sucessRate', dataDailySalesChart, optionsDailySalesChart);
 
 
-    /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
 
-    const dataCompletedTasksChart: any = {
-      labels: ['12p', '3p', '6p', '9p', '12p', '3a', '6a', '9a'],
+    /**
+     *   Validation Donut Init
+     */
+
+    const validationDonut = new Chartist.Pie('#ct-chart-donut', {
+      series: [ {value: 50,className: "test"},{value: 20,className: "test2"}]
+    }, {
+      donut: true,
+      donutWidth: 60,
+      startAngle: 270,
+      showLabel: true
+    });
+
+
+    /**
+     *   Number Of Students For Every Country
+     */
+
+    const numberOfStudents = new Chartist.Bar('#numberOfStudents', {
+      labels: ['Country 1', 'Country 2', 'Country 3', 'Country 4'],
       series: [
-        [230, 750, 450, 300, 280, 240, 200, 190]
+        [6, 4, 8, 7],
+        [4, 3, 7, 12],
+        [8, 3, 1, 6]
       ]
-    };
-
-    const optionsCompletedTasksChart: any = {
-      lineSmooth: Chartist.Interpolation.cardinal({
-        tension: 0
-      }),
-      low: 0,
-      high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-      chartPadding: { top: 0, right: 0, bottom: 0, left: 0 }
-    }
-
-    const completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
-
-    // start animation for the Completed Tasks Chart - Line Chart
-    this.startAnimationForLineChart(completedTasksChart);
-
-
-
-    /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
-
-    const datawebsiteViewsChart = {
-      labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
-      series: [
-        [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
-
-      ]
-    };
-    const optionswebsiteViewsChart = {
+    }, {
+      seriesBarDistance: 15,
       axisX: {
-        showGrid: false
+        offset: 20
       },
-      low: 0,
-      high: 1000,
-      chartPadding: { top: 0, right: 5, bottom: 0, left: 0 }
-    };
-    const responsiveOptions: any[] = [
-      ['screen and (max-width: 640px)', {
-        seriesBarDistance: 5,
-        axisX: {
-          labelInterpolationFnc: function (value) {
-            return value[0];
-          }
-        }
-      }]
-    ];
-    const websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
+      axisY: {
+        offset: 25,
+        labelInterpolationFnc: function(value) {
+          return value
+        },
+        scaleMinSpace: 20
+      }
+    });
 
-    // start animation for the Emails Subscription Chart
-    this.startAnimationForBarChart(websiteViewsChart);
+
+    /**
+     *   Number Of Succeed And Failure For Every Country
+     */
+
+      const numberSucceedFailure = new Chartist.Bar('#numberSucceedFailure', {
+      labels: ['Country 1', 'Country 2', 'Country 3', 'Country 4'],
+      series: [
+        [8, 12, 14, 13],
+        [2, 4, 5, 3],
+      ]
+    }, {
+      stackBars: true,
+      axisY: {
+        labelInterpolationFnc: function(value) {
+          return value;
+        }
+      }
+    }).on('draw', function(data) {
+      if(data.type === 'bar') {
+        data.element.attr({
+          style: 'stroke-width: 30px'
+        });
+      }
+    });
+
   }
 
 }
