d697596ebc02b9246e7533a5c2d0c180149665d5
ColombanThomas <thomas.colomban@etu.unice.fr>
2019-05-15 18:19:19 +0200

    Adding country graph on "validé" tab

--- my-app/src/app/etudiant-valide/etudiant-valide.component.ts
+++ my-app/src/app/etudiant-valide/etudiant-valide.component.ts
@@ -8,11 +8,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from 'app/user-profile/user-profile.component';
 import { Etudiant } from '../models/Etudiant';
 import { EtudiantSimp } from '../models/EtudiantSimp';
+import {StatistiquesService} from "../service/statistiques/statistiques.service";
+import * as Chartist from "chartist";
 
 
 @Component({
   selector: 'app-etudiant-valide',
-  providers: [TableListService],
+  providers: [TableListService,StatistiquesService],
   templateUrl: './etudiant-valide.component.html',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./etudiant-valide.component.css']
@@ -41,6 +43,9 @@ export class EtudiantValideComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngOnInit() {
+
+    this.initStudentByCountry();
+    //this.initPieChart('SI3');
 
     this.tableListService.getEtudiantObs(this.route.snapshot.queryParams).subscribe(rep => {
 
@@ -170,7 +175,96 @@ export class EtudiantValideComponent implements OnInit {
     this.router.navigate(['user-profile', { idEtudiant: etu.idEtudiant }]);
   }
 
-  constructor(private tableListService: TableListService,
+  /**
+   *   Number Of Students For Every Country
+  */
+  initStudentByCountry() {
+    const countryPro = this.statistiquesService.getNumberStudents('0').toPromise();
+
+    Promise.all([countryPro]).then((value) => {
+
+      const country1 = value[0][0].pays;
+      const country2 = value[0][1].pays;
+      const country3 = value[0][2].pays;
+
+      // console.log("country1" + country1);
+      // console.log("country2" + country2);
+      // console.log("country3" + country3);
+
+      const country11Pro = this.statistiquesService.getNumberStudentsWithCountry(country1,'0').toPromise();
+      const country22Pro = this.statistiquesService.getNumberStudentsWithCountry(country2,'0').toPromise();
+      const country33Pro = this.statistiquesService.getNumberStudentsWithCountry(country3,'0').toPromise();
+
+      Promise.all([country11Pro, country22Pro, country33Pro]).then((values) => {
+
+        console.log("values "+values[2]);
+
+        values[0].sort(this.sortByName);
+        values[1].sort(this.sortByName);
+        values[2].sort(this.sortByName);
+
+
+        const country11 = [values[0][0],values[0][1],values[0][2]];
+        const country22 = [values[1][0],values[1][1],values[1][2]];
+        const country33 = [values[2][0],values[2][1],values[2][2]];
+
+        console.log("values",country11[0], country11[1], country11[2]);
+        console.log("values",country22[0]);
+        console.log("values",country33[0]);
+
+        const numberOfStudents = new Chartist.Bar('#numberOfStudents', {
+          labels: [country11[0].pays, country22[0].pays, country33[0].pays],
+          series: [
+            [country11[0].nombre, country22[0].nombre, country33[0].nombre],
+            [country11[1].nombre, country22[1].nombre, country33[1].nombre],
+            [country11[2].nombre, country22[2].nombre, country33[2].nombre]
+          ]
+        }, {
+          seriesBarDistance: 15,
+          axisX: {
+            offset: 20
+          },
+          axisY: {
+            offset: 25,
+            labelInterpolationFnc: function (value) {
+              return value
+            },
+            scaleMinSpace: 20
+          }
+        });
+      });
+    });
+  }
+
+  sortByName(elementOne: any, elementTwo: any){
+    if (elementOne.nom_fr_fr<elementTwo.nom_fr_fr){
+      return 0;
+    }
+    else{
+      return 1;
+    }
+  }
+
+
+
+  /**
+   *   Succeed Graph Init
+   */
+  initPieChart(promo: string) {
+    const promotionPro = this.statistiquesService.getPieChart(promo).toPromise();
+
+    promotionPro.then((value) => {
+
+      const validationDonut = new Chartist.Pie('#ct-chart-pie', {
+        series: [value[0].degre, value[1].degre, value[2].degre]
+      }, {
+        startAngle: 270,
+        showLabel: true
+      });
+    });
+  }
+
+  constructor(private tableListService: TableListService,private statistiquesService: StatistiquesService,
     private route: ActivatedRoute,
     private domSanitizer: DomSanitizer,
     private router: Router) { }
