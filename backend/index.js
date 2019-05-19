3a95476e9f92015cc10b46e6e4a7b6f07e4e9302
ColombanThomas <thomas.colomban@etu.unice.fr>
2019-05-19 16:28:02 +0200

    Adding Update student

--- backend/index.js
+++ backend/index.js
@@ -150,6 +150,51 @@ app.get('/getData/numbersucceedcountry/:pays', function (req, res) {
   });
 });
 
+
+app.get('/getData/getidcountry/:country', function (req, res) {
+  let country = req.param('country');
+
+  con.query("SELECT id FROM renkinjutsushi.pays where nom_fr_fr = ?; \n\n", country, function (err, result, fields) {
+    if (err) {
+      console.log('Error 2.2 =\n', err);
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+
+app.get('/getData/getidspeciality/:speciality', function (req, res) {
+  let speciality = req.param('speciality');
+
+  con.query("SELECT idSpecialite FROM renkinjutsushi.specialite where nomSpecialite = ? ;; \n\n", speciality, function (err, result, fields) {
+    if (err) {
+      console.log('Error 2.2 =\n', err);
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+
+app.get('/getData/getidetat/:etat', function (req, res) {
+  let etat = req.param('etat');
+
+  con.query("SELECT idEtat FROM renkinjutsushi.etat where nomEtat = ? ;; \n\n", etat, function (err, result, fields) {
+    if (err) {
+      console.log('Error 2.2 =\n', err);
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+
+
+
+
+
+
 app.get('/getData', function (req, res) {
 
   var urlQuery = req.query;
@@ -308,10 +353,31 @@ app.get('/getData', function (req, res) {
 });
 
 app.post('/postData/updateStudent', (req, res) => {
-  
-  let student = Etudiant.create(req.body);
-    con.query("UPDATE renkinjutsushi.`etudiant` SET nom = ?, prenom = ?, promo = ?, specialite = ?, etat = ?, semestresRestants = ?, dateDebut = ?, dateFin = ?, pays = ?, obtenuVia = ?, mail = ?, annee = ? WHERE (idEtudiant = ?);\n;\n\n",
-      student.nom,student.prenom,student.promo,student.specialite,student.etat,student.semainesRestantes,student.dateDebut,student.dateFin,student.pays,student.obtenuVia,student.mail,student.annee,student.id, function (err, result, fields) {
+
+
+
+    const student = Etudiant.create(req.body);
+    console.log("student "+student.nom);
+
+    let dateD1 = student.dateDebut.toString().replace('T',' ');
+    let dateD2 = dateD1.replace('Z','');
+
+    let dateF1 = student.dateDebut.toString().replace('T',' ');
+    let dateF2 = dateD1.replace('Z','');
+
+
+
+    let params = [student.nom,student.prenom,student.promo,student.specialite.idSpecialite,student.commentaire,student.etat.idEtat,student.semainesRestantes,student.typeValidation,dateD2,dateF2,student.pays.idPays,student.obtenuVia,student.mail,student.annee,student.idEtudiant];
+    //res.status(201).json(ticket);
+
+    //res.status(200).json(SchoolTicket.update(req.params.schoolTicketId, req.body));
+
+    /*let student = Etudiant.create(req.body);
+    const updatedItem = Object.assign({}, req.body);
+    console.log("test : "+student);
+    console.log("test : "+updatedItem);*/
+    con.query("UPDATE `renkinjutsushi`.`etudiant` SET `nom` = ?, `prenom` = ?, `promo` = ?, `specialite` = ?, `commentaire` = ?, `etat` = ?, `semainesRestantes` = ?, `typeValidation` = ?, `dateDebut` = ?, `dateFin` = ?, `pays` = ?, `obtenuVia` = ?, `mail` = ?, `annee` = ? WHERE (`idEtudiant` = ?);\n\n\n",params,
+       function (err, result, fields) {
       if (err) {
         console.log('Error 2.2 =\n', err);
       } else {
